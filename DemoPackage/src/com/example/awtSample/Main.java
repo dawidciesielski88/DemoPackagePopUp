666097e1f5ee82367967d4223890f04b84c1bee5
Dawid Ciesielski <dawidciesielski88@gmail.com>
2020-05-28 11:43:36 +0200

    Add files via upload

---
+++ DemoPackage/src/com/example/awtSample/Main.java
@@ -0,0 +1,9 @@
+package com.example.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("My new pop up");
+        myWindow.setVisible(true);
+    }
+}
